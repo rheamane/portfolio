2fbeaa799562cf5537f2cd936534af6e17ab459a
Rhea Mane <95063614+orangejuice001@users.noreply.github.com>
2025-04-15 13:12:12 -0700

    config update

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-    output: "export",
-    reactStrictMode: true,
-    trailingSlash: true, 
-    basePath: "/portfolio-website",
+const nextConfig: NextConfig = {    
 };
 
 export default nextConfig;
